c4a64512bf3ad141a15700051f65218dca235dd6
dogutesting <dgknsynnn@gmail.com>
2023-05-26 17:08:03 +0300

    3. aşama, düzenlenebilir notlar.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 //#logo h1 title change
 
+console.log("Loaded");
+
+var isEnterFree;
+
+function makeEditable(element, bool) {
+    element.contentEditable = true;
+    isEnterFree = bool;
+    //burada mysql'e kaydetme işlemini başlatman lazım
+    //Anında başlatma ama yazma işlemi bittiğinde başlat
+    //sync'yide çevir
+}
+
+function handleBlur() {
+    //onBlur=
+    alert("Edit sona erdi");
+}
+
+function handleKeyDown(event) {
+    if (event.keyCode === 13) {
+        //event.preventDefault();
+        //return false;
+        event.target.blur();
+    }
+}
