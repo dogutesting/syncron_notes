faafb8b3352f5ba8a8121381b7838bae225685ef
unknown <dgknsynnn@gmail.com>
2023-05-31 19:27:08 +0300

    Responsive Notes Ve Bazı Hatalar

--- index.js
+++ index.js
@@ -4,21 +4,49 @@ console.log("Loaded");
 
 var isEnterFree;
 
-function makeEditable(element, bool) {
-    
+function handleBlurOnH3(element) {
+    element.contentEditable = false;
+    /* console.log("Edit sona erdi"); */
     element.contentEditable = true;
-    isEnterFree = bool;
-    //burada mysql'e kaydetme işlemini başlatman lazım
-    //Anında başlatma ama yazma işlemi bittiğinde başlat
-    //sync'yide çevir
 }
 
-function handleBlur() {
-    //onBlur=
-    alert("Edit sona erdi");
+function handleBlurOnP(element) {
+    if(element.classList.contains("main_card")) {
+        return;
+    }
+    else {
+        element.contentEditable = false;
+        /* console.log("Edit sona erdi"); */
+        element.contentEditable = true;
+    }
+    
 }
 
-function handleKeyDown(event) {
+function enableEditingForP(element, event) {
+    console.log(element);
+    event.stopPropagation();
+
+    // main_card elementine tıklandığında p elementini seçiyoruz
+    var pElement = element.querySelector('p');
+
+    // p elementini contenteditable yapmak ve odaklanmak için
+    pElement.focus();
+
+    // imleci p elementinin sonuna taşıyoruz
+    var range = document.createRange();
+    range.selectNodeContents(pElement);
+    range.collapse(false);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
+
+function fuckOffPropagation(event) {
+    event.stopPropagation();
+}
+
+
+function enterDisable(event) {
     if (event.keyCode === 13) {
         //event.preventDefault();
         //return false;
