59e926b1af73c422bf23988720465c079fce242e
unknown <dgknsynnn@gmail.com>
2023-06-01 19:18:42 +0300

    Taşıma için commit

--- index.js
+++ index.js
@@ -1,55 +1,70 @@
-//#logo h1 title change
+    //#logo h1 title change
 
-console.log("Loaded");
-
-var isEnterFree;
-
-function handleBlurOnH3(element) {
-    element.contentEditable = false;
-    /* console.log("Edit sona erdi"); */
-    element.contentEditable = true;
-}
-
-function handleBlurOnP(element) {
-    if(element.classList.contains("main_card")) {
-        return;
-    }
-    else {
+    function handleBlurOnH3(element) {
         element.contentEditable = false;
         /* console.log("Edit sona erdi"); */
         element.contentEditable = true;
     }
-    
-}
 
-function enableEditingForP(element, event) {
-    console.log(element);
-    event.stopPropagation();
-
-    // main_card elementine tıklandığında p elementini seçiyoruz
-    var pElement = element.querySelector('p');
-
-    // p elementini contenteditable yapmak ve odaklanmak için
-    pElement.focus();
-
-    // imleci p elementinin sonuna taşıyoruz
-    var range = document.createRange();
-    range.selectNodeContents(pElement);
-    range.collapse(false);
-    var selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-}
-
-function fuckOffPropagation(event) {
-    event.stopPropagation();
-}
-
-
-function enterDisable(event) {
-    if (event.keyCode === 13) {
-        //event.preventDefault();
-        //return false;
-        event.target.blur();
+    function handleBlurOnP(element) {
+        if(element.classList.contains("main_card")) {
+            return;
+        }
+        else {
+            element.contentEditable = false;
+            /* console.log("Edit sona erdi"); */
+            element.contentEditable = true;
+        }
+        
     }
-}
+
+    function enableEditingForP(element, event) {
+        event.stopPropagation();
+
+        //main_card elementine tıklandığında p elementini seçiyoruz
+        var pElement = element.querySelector('p');
+
+        //p elementine odaklanmak için
+        pElement.focus();
+
+        //p elementinin sonuna gelmek için
+        var range = document.createRange();
+        range.selectNodeContents(pElement);
+        range.collapse(false);
+        var selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+
+    function fuckOffPropagation(event) {
+        event.stopPropagation();
+    }
+
+    function enterDisable(event) {
+        if (event.keyCode === 13) {
+            event.target.blur();
+        }
+    }
+
+    function startScrolling(element) {
+        console.log(element);
+
+        textWidth = element.scrollWidth;
+        containerWidth = element.clientWidth;
+
+        /* if (textWidth > containerWidth) {
+            element.style.animation = "scrollText " + (textWidth / 50) + "s linear infinite";
+        } */
+    }
+
+    var scrollInterval;
+
+    function stopScrolling(element) {
+        scrollInterval = setInterval(function() {
+            element.scrollLeft += 1;
+        }, 20);
+    }
+
+    function stopScrolling() {
+        clearInterval(scrollInterval);
+    }
